c6493f1fb0359071f9266bef3f0406017a28a88e
alexyang4358 <alexyang4358@gmail.com>
2023-06-22 19:05:45 -0400

    hi

--- src/components/CardFeed.jsx
+++ src/components/CardFeed.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const CardFeed = ({ token }) => {
-  const [cards, setCards] = useState("");
+  const [cards, setCards] = useState([]);
 
   const baseURL = "https://cards-q6a8.onrender.com/";
 
@@ -24,6 +24,13 @@ const CardFeed = ({ token }) => {
     <>
       <div>
         <h1>Card Feed</h1>
+        <div className="containter">
+          <div className="card>">
+            {cards.map((card) => (
+              <ul>{card.headline}</ul>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
