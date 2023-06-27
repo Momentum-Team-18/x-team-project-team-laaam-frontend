43caaaeda3f34bcfaee3c6a5457d416bc8fe40b2
annamaynard <anna.maynard@gmail.com>
2023-06-27 15:19:10 -0400

    cardfeed update, userprofile

--- src/components/CardFeed.jsx
+++ src/components/CardFeed.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
 const CardFeed = ({ token }) => {
   const [cards, setCards] = useState([]);
-
+  const [userProfile, setUserProfile] = ("");
   const baseURL = "https://cards-q6a8.onrender.com/";
 
   useEffect(() => {
@@ -19,16 +18,33 @@ const CardFeed = ({ token }) => {
       });
   }, [token]);
 
+//    useEffect(() => {
+//     axios 
+//         .get(`${baseURL}api/profile/${userID}`)
+//             headers: {
+//                 Authorization: `Token ${token}`,
+//             },
+//         });
+//         .then((res) => {
+//             setUserProfile(response.data.id);
+//         })
+//    }), [token, userID]);
+
+
   // console.log(cards);
+
+  const handleUserProfileId = (id) => {
+    setUserProfile(id);
+    console.log("Hi- Im getting User ID");
+  };
 
   return (
     <>
-     
       <div>
         <h1>Card Feed!!</h1>
         <div className="container">
           <div>
-            {cards.map((card) => (
+            {cards.map((card,user) => (
               <ul
                 style={{
                   backgroundColor: card.background_color,
@@ -42,8 +58,12 @@ const CardFeed = ({ token }) => {
                 <h1>{card.headline}</h1>
                 <p>{card.front_text}</p>
                 <p>{card.date_created}</p>
-                <p>Created by: {card.sent_by_user}</p>
-                <p>Sent to: {card.sent_to_user}</p>
+                {/* <Link to="/userprofile" onClick={handleClickUserProfileId}></Link> */}
+                {/* <button onClick={() => handleUserProfileId({card.sent_by_user})}>
+                  Created by:{card.sent_by_user}
+                </button> */}
+                <br></br>
+                <button>Sent to: {card.sent_to_user}</button>
               </ul>
             ))}
           </div>
@@ -54,3 +74,4 @@ const CardFeed = ({ token }) => {
 };
 
 export default CardFeed;
+
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+// GET USER NAME --> user id --- so it shows up in URL  /ID (USERNAME?)
 
+/* ON THIS PAGE WE WANT TO DISPLAY :
+  - PUBIC USER INFO 
+            -USER NAME 
+            -AVATAR 
+   - CARDS USER HAS MADE 
+   - CARDS USER HAS RECEIVED???   --- IS THAT GONNA SHOW UP IN THE SAME FEED OR 
+                                     DO WE WANT TO TOGGLE BETWEEN SET AND RECEIVED MAPS/FEEDS IN USER VIEW
+            
 
+            */
 
 const UserProfile = ({token}) => {
     return (
 
         <h1>UserProfile</h1>
+
         
     )}
 
