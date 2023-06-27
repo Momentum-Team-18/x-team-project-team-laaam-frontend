c5ec4d653e4646ad63855f7be8bfc20032949131
annamaynard <anna.maynard@gmail.com>
2023-06-27 16:49:30 -0400

    userprofile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,6 +1,3 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
 // GET USER NAME --> user id --- so it shows up in URL  /ID (USERNAME?)
 
 /* ON THIS PAGE WE WANT TO DISPLAY :
@@ -14,11 +11,51 @@ import {Link} from 'react-router-dom'
 
             */
 
-const UserProfile = ({token}) => {
-    return (
 
-        <h1>UserProfile</h1>
 
+import React, { useState, useEffect } from "react";
+import { FaUserCircle } from "react-icons/fa";
+
+
+
+
+const UserProfile = () => {
+
+// const [userProfile, setUserProfile] = ("");
+//   const baseURL = "https://cards-q6a8.onrender.com/";
+
+//   useEffect(() => {
+//     axios
+//       .get(`${baseURL}api/profile/<int:pk>/`, {
+//         headers: {
+//           Authorization: `Token ${token}`,
+//         },
+//       })
+//       .then((res) => {
+//         setCards(res.data);
+//       });
+//   }, [token]);
+  
+return (
+<>
+        <div>
+        <h1> U S E R N A M E</h1>
+        <FaUserCircle className="avatar-userprofile"/>
+        <br></br>
+        <br></br>
+        <button> FOLLOW </button>
+        <br></br>
+        </div>
+        <div className="friend-links-container">
+            <button className="following-btn">PEROPLE USER FOLLOW</button>
+            <button className="following-btn">PEOPLE WHO FOLLOW USER</button>
+        </div>
+        <div className="userCardFeedcontainer"> 
+            <h1>DISPLAY OF CARDS USER HAS MADE</h1>
+            <h2>toggel between</h2>
+            <h1>DISPLAY OF CARDS USER HAS RECEIVED</h1>
+        </div>
+</>
         
     )}
 
