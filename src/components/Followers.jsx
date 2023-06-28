cba7834b6bce7de7249fae9b7c77a2fdfcd36a00
annamaynard <anna.maynard@gmail.com>
2023-06-28 15:53:45 -0400

    friends list followers list

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { FaUserCircle } from "react-icons/fa";
 
 const Followers = ({ token }) => {
   const baseURL = "https://cards-q6a8.onrender.com/";
@@ -8,7 +9,7 @@ const Followers = ({ token }) => {
 
   useEffect(() => {
     axios
-      .get(`${baseURL}api/cards/received/`, {
+      .get(`${baseURL}api/user_followers/`, {
         headers: {
           Authorization: `Token ${token}`,
         },
@@ -18,13 +19,16 @@ const Followers = ({ token }) => {
       });
   }, [token]);
 
+
   return (
     <>
       <div className="followers">
         {followers.map((follower) => (
-          <ul className="card" key={follower.id}>
-            <h1>{follower.id}</h1>
-            <p>Followers: {follower.sent_to_user}</p>
+          <ul className="follow-card" key={follower.id}>
+            <FaUserCircle className="avatar-userprofile-follow" />
+            <h1>{follower.this_user}</h1>
+            <button className="follow-unfollow-btn">Remove</button>
+            {/* Delete in button */}
           </ul>
         ))}
       </div>
--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,8 +1,47 @@
-const FriendList = () => {
+import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { FaUserCircle } from "react-icons/fa";
+
+const FriendList = ({ token }) => {
+  const baseURL = "https://cards-q6a8.onrender.com/";
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${baseURL}api/user_following/`, {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      })
+      .then((res) => {
+        setFollowers(res.data);
+      });
+  }, [token]);
+
+//   useEffect(() => {
+//     axios
+//       .delete(`${baseURL}api/unfollow_user/${follower.id}`, {
+//         headers: {
+//           Authorization: `Token ${token}`,
+//         },
+//       })
+//   }, [token]);
+
+
   return (
-    <h1>FriendList (friends of user)</h1>
-    // User Can not edit
-    // But user could follow from this list?
+    <>
+      <div className="followers">
+        {followers.map((follower) => (
+          <ul className="follow-card" key={follower.id}>
+            <FaUserCircle className="avatar-userprofile-follow" />
+            <h1>{follower.user_this_user_is_following}</h1>
+            <button className="follow-unfollow-btn">Remove</button>
+            {/* Delete in button */}
+          </ul>
+        ))}
+      </div>
+    </>
   );
 };
 
