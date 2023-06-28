efb00b780f20a2f73482cfa7d1c78df68dbfa78e
alexyang4358 <alexyang4358@gmail.com>
2023-06-28 13:02:45 -0400

    start

--- src/components/CardEdit.jsx
+++ src/components/CardEdit.jsx
@@ -1,14 +1,13 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const CardEdit = () => {
-    return (
-        
-        <section className='section'>
-            <h2>Edit</h2>
+  return (
+    <section className="section">
+      <h2>Edit</h2>
 
-            <Link to='/cardedit'>Edit Card</Link>
-        </section>
-    )
-    }
-    
-    export default CardEdit;
+      <Link to="/cardedit">Edit Card</Link>
+    </section>
+  );
+};
+
+export default CardEdit;
